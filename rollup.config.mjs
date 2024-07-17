68a56fa9295340eeda729ebdc70f4a48f012eef6
Maximalsparen <maximalsparen@gmail.com>
2024-07-17 20:59:47 +0200

    replaced deprecated plugin rollup-plugin-terser to @rollup/plugin-terser changed rollup config name to build package changed rollup config to use new @rollup/plugin-terser package update rollup package to latest created new dist build

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,6 +1,6 @@
-import typescript from 'rollup-plugin-typescript2';
+import terser from '@rollup/plugin-terser';
 import postcss from 'rollup-plugin-postcss';
-import { terser } from 'rollup-plugin-terser';
+import typescript from 'rollup-plugin-typescript2';
 
 export default [
     {
